087d8c89f2464b8df2875cf7b2f7d7bfd44b2d3a
Ali Hajivand [ MTNIrancell - Labs ] <ali.hajiv@mtnirancell.ir>
2025-03-08 03:02:56 +0330

    پیام کامیت خود را اینجا بنویسید

--- test/Program.cs
+++ test/Program.cs
@@ -7,21 +7,21 @@ public class Program
     public static void Main()
     {
 
-        SqlConnection connection = new();
-        connection.ConnectionString = "Data Source=.;Initial Catalog=ACademy;Integrated Security=True";
-        connection.Open();
+        // SqlConnection connection = new();
+        // connection.ConnectionString = "Data Source=.;Initial Catalog=ACademy;Integrated Security=True";
+        // connection.Open();
 
-        SqlCommand command = new SqlCommand();
+        // SqlCommand command = new SqlCommand();
 
-        command.CommandText = "select * from student ";
-        command.Connection = connection;
+        // command.CommandText = "select * from student ";
+        // command.Connection = connection;
 
-        SqlDataReader sqlDataReader = command.ExecuteReader();
-        while (sqlDataReader.Read()) {
-            Console.WriteLine($"{sqlDataReader["Name"]} {sqlDataReader["Family"]}");
-        }
+        // SqlDataReader sqlDataReader = command.ExecuteReader();
+        // while (sqlDataReader.Read()) {
+        //     Console.WriteLine($"{sqlDataReader["Name"]} {sqlDataReader["Family"]}");
+        // }
 
-        connection.Close();
+        // connection.Close();
 
     }
 
